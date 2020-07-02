8102b1496b934682a079d5d614a76a0f1ba4e7b6
Kate-Shin <sdk02307@gmail.com>
2020-07-02 19:29:28 +0900

    20200702 프로젝트 조회 수정

--- front/src/views/knm/KAKNM0103P1.vue
+++ front/src/views/knm/KAKNM0103P1.vue
@@ -1,5 +1,14 @@
 <template>
-  <KAKNM0103Form></KAKNM0103Form>
+  <v-dialog
+    v-model="dialog"
+    persistent
+    max-width="100%"
+  >
+<!--   <KAKNM0103Form @close="() => {isDialog=false}"></KAKNM0103Form> -->
+<KAKNM0103Form @close="close" @checkedbtn="checkedbtn"></KAKNM0103Form>
+ <!-- <v-btn @click.prevent="close">취소</v-btn> -->
+
+  </v-dialog>
 </template>
 
 <script>
@@ -11,7 +20,17 @@ export default {
   },
   data () {
     return {
-
+      isDialog: ''
+    }
+  },
+  props: ['dialog'],
+  methods: {
+    close () {
+      console.log('parent-close')
+      this.$emit('close')
+    },
+    checkedbtn (data1, data2) {
+      console.log('data', data1, data2)
     }
   }
 }
