3457e362ab223c4241be9a9cae0b34fa0c6232b5
yeeun0514 <yeeun.g.sin@gmail.com>
2020-07-18 11:43:39 +0900

    / file upload 완성

---
+++ front/src/components/knd/FILEDOWNLOADForm.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <!-- <input ref="fileTag" type="file" multiple="multiple" @change="selectFile"/>
+        <v-btn class="btn btn-success"  :disabled="!selectedFiles.length" @click.prevent="upload">
+            Upload
+        </v-btn> -->
+        <ul>
+            <li v-for="(list, index) in lists" :key="index">
+                <a @click="fileClick(list.file_id)">{{ list.org_file_name }}</a>
+            </li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+import { getFileList } from '@/api/File.js'
+
+export default {
+  name: 'FILEUPLOADForm.vue',
+  components: {
+
+  },
+  data: () => {
+    return {
+      lists: '',
+      postId: 'id'
+    }
+  },
+  created () {
+    this.getList()
+  },
+  methods: {
+    selectFile () {
+      console.log('select File 함수')
+      this.selectedFiles = this.$refs.fileTag.files
+      console.log('selectFile << file >> : ', this.$refs.fileTag.files)
+    },
+    async getList () {
+      const a = {
+        params: {
+          postId: this.postId
+        }
+      }
+      const { data } = await getFileList(a)
+      this.lists = data
+    },
+    fileClick (fileId) {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
