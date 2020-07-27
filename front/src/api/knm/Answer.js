25aa6ebeea29974c4dba3eca753f3d7121858e26
yeeun0514 <yeeun.g.sin@gmail.com>
2020-07-27 15:36:22 +0900

    api 추가

--- front/src/api/knm/Answer.js
+++ front/src/api/knm/Answer.js
@@ -7,15 +7,20 @@ const instance = axios.create({
 function postAnswer (Data) {
   return instance.post('KAKNM02/write', Data)
 }
+// 수정을 위한 질문답변 정보 가져오기
+function getModifyDetail (formData) {
+  console.log('answer.js api 호출!!!!')
+  console.log(formData)
+  return instance.get('KAKNM02/getModifyDetail', { params: formData })
+}
 // 내가 쓴 답변 리스트
 function getAnsList (Data) {
   return instance.get('KAKNM02/getList', Data)
 }
 // 타이틀, 상태로 답변 조회
 function getAnsSearchList (Data) {
-  return instance.get('KAKNM02/searchList')
+  return instance.get('KAKNM02/searchList', Data)
 }
-
 // 답변 보기
 function getAnswer (Data) {
   return instance.get('KAKNM02/getAnswer', Data)
@@ -25,4 +30,4 @@ function delAnswer (Data) {
   return instance.get('KAKNM02/ansDelete', Data)
 }
 
-export { postAnswer, getAnsList, getAnsSearchList, getAnswer, delAnswer }
+export { postAnswer, getAnsList, getAnsSearchList, getAnswer, delAnswer, getModifyDetail }
