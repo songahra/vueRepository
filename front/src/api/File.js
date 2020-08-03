3fd9f4d17ad3fc7769a9c18886e76a9031a6f1ed
yeeun0514 <yeeun.g.sin@gmail.com>
2020-08-03 10:56:10 +0900

    파일 다운로드 api

--- front/src/api/File.js
+++ front/src/api/File.js
@@ -11,5 +11,10 @@ function postFile (formData) {
 function getFileList (Data) {
   return instance.get('/file/list', Data)
 }
+function download (data) {
+  return axios.post(process.env.VUE_APP_API_URL + '/file/download', data, {
+    responseType: 'blob'
+  })
+}
 
-export { postFile, getFileList }
+export { postFile, getFileList, download }
